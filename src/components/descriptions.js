86e6ebbc9060d93ed4d36536089862362456476c
vidallizet <lvidalsaldivar7855@richmondhillhs.org>
2021-11-04 15:37:54 +0000

    vidallizet

--- src/components/descriptions.js
+++ src/components/descriptions.js
@@ -8,17 +8,17 @@ function Descriptions() {
         <div className = 'col'>
           <h5 className = 'title-text'>Real Cats</h5>
           <p className = 'sub-text'>
-            Contrary to popular belief, cats aren't loners. At furbook, we know cats need friendship, affection, and comradery. It is 						  our core belief that beneath every cat's hard, angry, exterior there is a pet who jus wants to play and cuddle with a						        friend</p>
+            Contrary to popular belief, cats aren't loners. At furbook, we know cats need friendship, affection, and comradery. It is 						  our core belief that beneath every cat's hard, angry, exterior there is a pet who jus wants to play and cuddle with a friend. they are great companions and may relieve your stress once you come home.</p>
         </div>
         <div className = 'col'>
           <h5 className = 'title-text'>Real Dogs</h5>
           <p className = 'sub-text'>
-            Dogs are man's best friend. But dogs can also be dog's best friend. Maybe even bester than man's best friend. On furbook, 					      you can find millions of other dogs who can be a loyal companion, trusted confidant, and reliable wrestling partner</p>
+            Dogs are a man's best friend. But dogs can also be dog's best friend. Maybe even bester than a man's best friend. On furbook, 					      you can find millions of other dogs who can be a loyal companion, trusted confidant, and reliable wrestling partner. Their naturally optimistic spirits help us relieve many stress and it's also entertaining to watch how a dog's behavior varies from being with you and with other dogs.</p>
         </div>
         <div className = 'col'>
           <h5 className = 'title-text'>Real Friends</h5>
           <p className = 'sub-text'>s
-            Who says cats and dogs can't get along? Here at Furbook, we believe all pets can be best friends. Our furbook users 					         believe it doesn't matter if you are a cat, dog, rabbit, or ferret; underneath that fur we are all cute, wonderful pets.					 </p>
+            Who says cats and dogs can't get along? Here at Furbook, we believe all pets can be best friends. Our furbook users 					         believe it doesn't matter if you are a cat, dog, rabbit, or ferret; underneath that fur we are all cute, wonderful pets. Of course, each pet has it's differences but they can learn to get along with other pets if shown the right way. 					 </p>
          </div>
       </div>
     </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ import './css/navbar.css';
 function Navbar() {
   return(
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">Furbook</a>
+      <a className="navbar-brand" href="-">Furbook</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
       </button>
@@ -12,13 +12,13 @@ function Navbar() {
       <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
         <li className="nav-item active">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-link" href="-">Home <span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
-        <a className="nav-link" href="/">Post</a>
+        <a className="nav-link" href="-">Post</a>
         </li>
         <li className="nav-item">
-        <a className="nav-link" href="/">Pet</a>
+        <a className="nav-link" href="-">Pet</a>
         </li>
       </ul>
       <form className="form-inline my-2 my-lg-0">
